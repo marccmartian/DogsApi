fd96fcbe0601e3102aff6c061b11e687e90224be
Marvin Carrasco <marccenvivo@gmail.com>
2020-07-07 21:18:35 -0500

    add loading mesage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import RandomDogs from './components/RandomDogs';
 import SelectBreed from './components/SelectBreed';
+import Load from './components/Load';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
   const [images, setImages] = useState([]);
 
   //request only one breed
   useEffect(() => {
+    setLoading(true)
     const getBreed = async (breed) => {
       const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/10`);
       const data = await response.json();
       setImages(data.message);
+      setLoading(false);
     }
-    if (!breed) return;
+    //si no hay data en breed retorna nada, entonces se muestra las imgenes random
+    if (!breed) {
+      setLoading(false);
+      return;
+    };
     getBreed(breed);
   }, [breed]);
 
@@ -43,7 +51,9 @@ function App() {
     <div className="App">
       <h1>Find a Dog</h1>
       <SelectBreed breeds={breeds} selectedBreed={setBreed} />
-      <RandomDogs images={images} />
+      {
+        loading ? <Load /> : <RandomDogs images={images} />
+      }
     </div>
   );
 }
---
+++ src/components/Load.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Load = () => {
+  return (
+    <div>LOADING, please weit...</div>
+  )
+}
+
+export default Load;
